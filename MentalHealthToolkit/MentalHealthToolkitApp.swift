e782b7702e61b34de37a4240430bcdf076cde3ba
Santiago Cento <cento.santiago@gmail.com>
2025-03-08 15:44:31 -0300

    Initial Commit

---
+++ MentalHealthToolkit/MentalHealthToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalHealthToolkitApp.swift
+//  MentalHealthToolkit
+//
+//  Created by Santi on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MentalHealthToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MentalHealthToolkitTests/MentalHealthToolkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MentalHealthToolkitTests.swift
+//  MentalHealthToolkitTests
+//
+//  Created by Santi on 08/03/2025.
+//
+
+import Testing
+@testable import MentalHealthToolkit
+
+struct MentalHealthToolkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
